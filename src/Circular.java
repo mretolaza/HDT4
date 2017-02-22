94dbc5aa693d9a40d1218a6bc33bed68e6efe9fa
ret16339 <ret16339@uvg.edu.gt>
2017-02-22 08:30:25 -0600

    ClaseListasDobles

---
+++ src/Circular.java
@@ -0,0 +1,127 @@
+/**
+ * @author María Mercedes Retolaza Reyna,16339
+ * Esta clase implementa las listas dinámicas circulares 
+ * @param <E>
+ */
+public class Circular<E> extends AbstractList<E> implements iStack<E>{
+
+	protected Nodo<E> tail;
+	protected int count;
+
+	/**
+	 * Constructor de la clase 
+	 */
+	public Circular(){
+		tail = null;
+		count = 0;
+	}
+
+	/**Metodo que añade un elemento a lista 
+	 * @param value
+	 */
+	public void addFirst(E value){
+		Nodo<E> temp = new Nodo<E>(value);
+		if (tail == null) { 
+			tail = temp;
+			tail.setNext(tail);
+		} else { 
+			temp.setNext(tail.next());
+			tail.setNext(temp);
+		}
+		count++;
+	}
+
+
+	/** Metodo que añade el ultimo elemento a la lista 
+	 * @param value 
+	 */
+	public void addLast(E value){
+
+		addFirst(value);
+		tail = tail.next();
+	}
+
+	/**Metodo que elimina un elemento de la lista 
+	 * @return
+	 */
+	public E removeLast(){
+		Nodo<E> finger = tail;
+		while (finger.next() != tail){
+			finger = finger.next();
+		}
+		Nodo<E> temp = tail;
+		if (finger == tail)
+		{
+			tail = null;
+		} else {
+			finger.setNext(tail.next());
+			tail = finger;
+		}
+		count--;
+		return temp.value();
+	}
+
+	/* (non-Javadoc)
+	 * @see Lista#size()
+	 */
+	@Override
+	public int size() {
+		throw new UnsupportedOperationException("Not supported yet."); 
+	}
+
+	/* (non-Javadoc)
+	 * @see Lista#addFirst()
+	 */
+	@Override
+	public void addFirst() {
+		throw new UnsupportedOperationException("Not supported yet."); 
+	}
+
+	/* (non-Javadoc)
+	 * @see Lista#addLast()
+	 */
+	@Override
+	public void addLast() {
+		throw new UnsupportedOperationException("Not supported yet."); 
+	}
+
+	/* (non-Javadoc)
+	 * @see Lista#getLast()
+	 */
+	@Override
+	public E getLast() {
+		throw new UnsupportedOperationException("Not supported yet."); 
+	}
+
+	/* (non-Javadoc)
+	 * @see iStack#push(java.lang.Object)
+	 */
+	@Override
+	public void push(E p) {
+		// new entry:
+		addFirst(p);
+		tail = tail.next();
+	}
+
+	/* (non-Javadoc)
+	 * @see iStack#pop()
+	 */
+	@Override
+	public E pop() {
+		Nodo<E> finger = tail;
+		while (finger.next() != tail) {
+			finger = finger.next();
+		}
+
+		Nodo<E> temp = tail;
+		if (finger == tail)
+		{
+			tail = null;
+		} else {
+			finger.setNext(tail.next());
+			tail = finger;
+		}
+		count--;
+		return temp.value();
+	}
+}
---
+++ src/Doble.java
@@ -0,0 +1,107 @@
+public class Doble<E> extends AbstractList<E> implements iStack<E>{
+protected int count;
+protected DoublyLinkedNode<E> head;
+protected DoublyLinkedNode<E> tail;
+
+
+public Doble()
+// post: constructs an empty list
+{
+   head = null;
+   tail = null;
+   count = 0;
+}
+
+    private Doble(E value, Doble<E> head, Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+public void addFirst(E value)
+// pre: value is not null
+// post: adds element to head of list
+{
+   // construct a new element, making it head
+   head = new DoublyLinkedNode<E>(value, head, null);
+   // fix tail, if necessary
+   if (tail == null) tail = head;
+   count++;
+}
+
+
+public void addLast(E value)
+// pre: value is not null
+// post: adds new value to tail of list
+{
+   // construct new element
+   tail = new DoublyLinkedNode<E>(value, null, tail);
+   // fix up head
+   if (head == null) head = tail;
+   count++;
+}
+
+
+public E removeLast()
+// pre: list is not empty
+// post: removes value from tail of list
+
+{
+   DoublyLinkedNode<E> temp = tail;
+   /*tail = tail.previous();
+   if (tail == null) {
+       head = null;
+   } else {
+       tail.setNext(null);
+   }
+   count--;
+   return temp.value();*/
+   return null;
+   
+}
+   
+
+
+    @Override
+    public int size() {
+        return count;
+    }
+
+    @Override
+    public void addFirst() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addLast() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public E getLast() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void push(E p) {
+        // construct a new element, making it head
+   tail = new DoublyLinkedNode<E>(p, null, tail);
+   // fix up head
+   if (head == null) head = tail;
+   count++;
+    }
+
+    @Override
+    public E pop() {
+    DoublyLinkedNode<E> temp = tail;
+    if(!isEmpty()){
+        if(head.nextElement==null){
+        head = null;
+        tail = null;
+    }
+    else{
+        tail = tail.previousElement;
+        tail.nextElement = null;
+    }}
+    
+    return (E)temp.value();
+}}
